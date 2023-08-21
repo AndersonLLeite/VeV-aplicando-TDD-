7364a674954c525275e0690970a63295a2c31cc9
Anderson dos Santos <anderson.santos.silva@ccc.ufcg.edu.br>
2023-08-21 20:45:28 -0300

    atingindo testes parcialmente

--- src/gerenciadordetarefas/TarefaController.java
+++ src/gerenciadordetarefas/TarefaController.java
@@ -2,18 +2,29 @@ package gerenciadordetarefas;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 public class TarefaController {
+    private Collection<Tarefa> tarefas;
+
+    public TarefaController() {
+        this.tarefas = new ArrayList<>();
+    }
 
     public Collection<Tarefa> getTarefas(){
-        return new ArrayList<>();
+        return tarefas;
     }
     public Tarefa getTarefa(String titulo){
+        for (Tarefa tarefa: tarefas) {
+            if (tarefa.getTitulo().equals(titulo)){
+                return  tarefa;
+            }
+        }
         return null;
     }
 
     public void addTarefa(Tarefa tarefa){
-
+        tarefas.add(tarefa);
     }
     public void updateTarefa(String titulo, String descricao, String dataVencimento, int prioridade){
 
