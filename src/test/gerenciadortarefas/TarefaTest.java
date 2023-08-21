be56f75968b7d62714d7e235cec5262ffbe551bc
Anderson dos Santos <anderson.santos.silva@ccc.ufcg.edu.br>
2023-08-21 20:15:55 -0300

    refatorado Tarefa com construtor

--- src/test/gerenciadortarefas/TarefaTest.java
+++ src/test/gerenciadortarefas/TarefaTest.java
@@ -13,7 +13,18 @@ class TarefaTest {
         String dataVencimento = "21/08/2023";
         int prioridade = 3;
 
-        Tarefa tarefa = new Tarefa();
+        Tarefa tarefa = new Tarefa(titulo, descricao, dataVencimento, prioridade);
+
+        assertTrue(tarefa.getTitulo().equals(titulo) && tarefa.getDescricao().equals(descricao) && tarefa.getDataVencimento().equals(dataVencimento) && tarefa.getPrioridade() == prioridade);
+    }
+    @Test
+    public void testCriarTarefa2(){
+        String titulo = "Tarefa2";
+        String descricao = "Outra tarefa valida";
+        String dataVencimento = "30/09/2024";
+        int prioridade = 2;
+
+        Tarefa tarefa = new Tarefa(titulo, descricao, dataVencimento, prioridade);
 
         assertTrue(tarefa.getTitulo().equals(titulo) && tarefa.getDescricao().equals(descricao) && tarefa.getDataVencimento().equals(dataVencimento) && tarefa.getPrioridade() == prioridade);
     }
