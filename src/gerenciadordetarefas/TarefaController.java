4e4b128ed40a4873afad07cfdfcba673f4e5078a
Anderson dos Santos <anderson.santos.silva@ccc.ufcg.edu.br>
2023-08-21 20:38:00 -0300

    criado entidade TarefaController e metodos necessarios

---
+++ src/gerenciadordetarefas/TarefaController.java
@@ -0,0 +1,24 @@
+package gerenciadordetarefas;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class TarefaController {
+
+    public Collection<Tarefa> getTarefas(){
+        return new ArrayList<>();
+    }
+    public Tarefa getTarefa(String titulo){
+        return null;
+    }
+
+    public void addTarefa(Tarefa tarefa){
+
+    }
+    public void updateTarefa(String titulo, String descricao, String dataVencimento, int prioridade){
+
+    }
+    public void deleteTarefa(String titulo){
+
+    }
+}
