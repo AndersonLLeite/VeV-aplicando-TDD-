1a728bff25ef2c218ed899a7d38779bb84b78bbb
Anderson dos Santos <anderson.santos.silva@ccc.ufcg.edu.br>
2023-08-21 20:03:55 -0300

    criado teste inicial para Tarefa

--- src/test/gerenciadortarefas/TarefaTest.java
+++ src/test/gerenciadortarefas/TarefaTest.java
@@ -1,7 +1,20 @@
 package test.gerenciadortarefas;
 
+import gerenciadordetarefas.Tarefa;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TarefaTest {
+    @Test
+    public void testCriarTarefa(){
+        String titulo = "Tarefa1";
+        String descricao = "Uma tarefa valida";
+        String dataVencimento = "21/08/2023";
+        int prioridade = 3;
 
+        Tarefa tarefa = new Tarefa();
+
+        assertTrue(tarefa.getTitulo().equals(titulo) && tarefa.getDescricao().equals(descricao) && tarefa.getDataVencimento().equals(dataVencimento) && tarefa.getPrioridade().equals(prioridade));
+    }
 }
