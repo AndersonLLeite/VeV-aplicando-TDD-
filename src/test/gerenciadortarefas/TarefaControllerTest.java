86fa20c42ed9cccd3fa696704231bfe09474c741
Anderson dos Santos <anderson.santos.silva@ccc.ufcg.edu.br>
2023-08-21 20:59:28 -0300

    criado testes para listagem de tarefas

--- src/test/gerenciadortarefas/TarefaControllerTest.java
+++ src/test/gerenciadortarefas/TarefaControllerTest.java
@@ -4,6 +4,10 @@ import gerenciadordetarefas.Tarefa;
 import gerenciadordetarefas.TarefaController;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TarefaControllerTest {
@@ -56,5 +60,34 @@ public class TarefaControllerTest {
         tarefaController.deleteTarefa(tarefa.getTitulo());
 
         assertNull(tarefaController.getTarefa("Tarefa"));
+    }
+    @Test
+    public void testListarTarefasVazio(){
+        TarefaController tarefaController = new TarefaController();
+
+        assertEquals(0, tarefaController.listarTarefas());
+    }
+    @Test
+    public void testListarTarefasUma(){
+        Tarefa tarefa = new Tarefa("Tarefa", "Uma nova tarefa", "27/09/2022", 2);
+        TarefaController tarefaController = new TarefaController();
+        tarefaController.addTarefa(tarefa);
+
+        assertEquals(tarefa, tarefaController.listarTarefas().get(1));
+    }
+    @Test
+    public void testListarTarefasMultiplas(){
+        Tarefa tarefa1 = new Tarefa("Tarefa1", "Primeira tarefa", "19/07/2023", 3);
+        Tarefa tarefa2 = new Tarefa("Tarefa1", "Segunda tarefa", "21/08/2023", 2);
+        Tarefa tarefa3 = new Tarefa("Tarefa1", "Terceira tarefa", "30/09/2024", 1);
+
+        TarefaController tarefaController = new TarefaController();
+        tarefaController.addTarefa(tarefa1);
+        tarefaController.addTarefa(tarefa2);
+        tarefaController.addTarefa(tarefa3);
+
+        List<Tarefa> listaCorreta = Arrays.asList(tarefa1, tarefa2, tarefa3);
+
+        assertEquals(listaCorreta, tarefaController.listarTarefas());
     }
 }
